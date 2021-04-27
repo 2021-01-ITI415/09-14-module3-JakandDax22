c62f7e2362ba177e3421ea6b5611efacac075ff3
JakandDax22 <60167934+JakandDax22@users.noreply.github.com>
2021-04-26 22:18:06 -0400

    Audio Lab

---
+++ Assets/2. Character Stats/Scripts/ScriptableObjects/PlayerAudio.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioClip splashSound;
+    public AudioSource audioS;
+    public AudioMixerSnapshot idleSnapshot;
+    public AudioMixerSnapshot auxInSnapshot;
+    public AudioMixerSnapshot ambidleSnapshot;
+    public AudioMixerSnapshot ambInSnapshot;
+    public LayerMask enemyMask;
+    bool enemyNear;
+
+    public void Update()
+    {
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, 5f, transform.forward, 0f, enemyMask);
+        
+        //New code starts here
+        if (hits.Length > 0)
+        {
+            enemyNear = true;
+        }
+        else
+        {
+            enemyNear = false;
+        }
+        if (!AudioManager.manager.eventRunning)
+        {
+
+            if (enemyNear)
+            {
+                if (AudioManager.manager.auxIn)
+                {
+                    auxInSnapshot.TransitionTo(0.5f);
+                    AudioManager.manager.currentAudioMixerSnapshot = auxInSnapshot;
+                    AudioManager.manager.auxIn = true;
+                }
+                else
+                {
+                    if (AudioManager.manager.currentAudioMixerSnapshot == AudioManager.manager.eventSnap)
+                    {
+                        auxInSnapshot.TransitionTo(0.5f);
+                        AudioManager.manager.currentAudioMixerSnapshot = auxInSnapshot;
+                        AudioManager.manager.auxIn = true;
+                    }
+                }
+            }
+            else
+            {
+                if (AudioManager.manager.auxIn)
+                {
+                    idleSnapshot.TransitionTo(0.5f);
+                    AudioManager.manager.currentAudioMixerSnapshot = idleSnapshot;
+                    AudioManager.manager.auxIn = false;
+                }
+                else
+                {
+                    if (AudioManager.manager.currentAudioMixerSnapshot == AudioManager.manager.eventSnap)
+                    {
+                        idleSnapshot.TransitionTo(0.5f);
+                        AudioManager.manager.currentAudioMixerSnapshot = idleSnapshot;
+                        AudioManager.manager.auxIn = false;
+                    }
+                }
+            }
+        }
+        //New code ends here
+        if (hits.Length > 0)
+        {
+            if (!enemyNear)
+            {
+                auxInSnapshot.TransitionTo(0.5f);
+                enemyNear = true;
+            }
+        }
+        else
+        {
+            if (enemyNear)
+            {
+                idleSnapshot.TransitionTo(0.5f);
+                enemyNear = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("EnemyZone"))
+        {
+            auxInSnapshot.TransitionTo(0.5f);
+        }
+        if (other.CompareTag("Ambience"))
+        {
+            ambInSnapshot.TransitionTo(0.5f);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("EnemyZone"))
+        {
+            idleSnapshot.TransitionTo(0.5f);
+        }
+        if (other.CompareTag("Ambience"))
+        {
+            ambidleSnapshot.TransitionTo(0.5f);
+        }
+    }
+}
---
+++ Assets/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager manager;
+
+    public AudioSource mainMusic;
+    public AudioSource auxMusic;
+    public AudioSource ambMusic;
+    public AudioSource eventMusic;
+
+
+    private void Awake()
+    {
+        manager = this;
+    }
+
+    public AudioMixerSnapshot eventSnap;
+    public AudioMixerSnapshot idleSnapshot;
+    public AudioMixerSnapshot currentAudioMixerSnapshot;
+
+    public bool eventRunning;
+    public bool auxIn;
+    public IEnumerator PlayEventMusic()
+    {
+        eventRunning = true;
+        eventSnap.TransitionTo(0.25f);
+        currentAudioMixerSnapshot = eventSnap;
+
+        yield return new WaitForSeconds(0.3f);
+        eventMusic.Play();
+        while (eventMusic.isPlaying)
+        {
+            yield return null;
+        }
+        eventRunning = false;
+        //idleSnapshot.TransitionTo(0.5f);
+        yield break;
+    }
+}
